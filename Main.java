2ae51194498c07f0fa2be46da54423dad2579e96
petub <114745589+petubb@users.noreply.github.com>
2022-11-07 09:25:24 -0400

    Correção de erros
    corrige uns erro ai

--- Main.java
+++ Main.java
@@ -1,71 +1,94 @@
-package caixaEletronico;
+package atividadeCaixaRafa;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
-public class Main {
+public class Caixa {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int notaDeCem = 100;
-		int notaDeCinquenta = 50;
-		int notaDeVinte = 20;
-		int notaDeDez = 10;
-		int notaDeCinco = 5;
-		int notaDeDois = 2;
-		int notaDeUm = 1;
+		Integer valor;
 
-		System.out.println("Quanto deseja retirar?");
-		Integer valor = sc.nextInt();
+		Integer notaUm = 1;
+		Integer notaDois = 2;
+		Integer notaCinco = 5;
+		Integer notaDez = 10;
+		Integer notaVinte = 20;
+		Integer notaCinquenta = 50;
+		Integer notaCem = 100;
 
-		if(valor >= notaDeCem) {
-			notaDeCem = valor / 100;
-			valor = valor % 100 ;
-			if (notaDeCem != 0) 
-				System.out.println(notaDeCem + " notas de R$ 100,00");
+		System.out.println("Insira o valor que deseja retirar");
+		valor = sc.nextInt();
+
+		if(valor >= notaCem) {
+			notaCem = valor / notaCem;
+			valor = valor % 100;
+			if(notaCem == 1) {
+				System.out.println(notaCem + " nota de R$ 100,00");
+			}
+			else if (notaCem != 0)
+				System.out.println(notaCem + " notas de R$ 100,00");
 		}
 
-		if(notaDeCinquenta >= notaDeCinquenta) {
-			notaDeCinquenta = valor / 50;
+		if(valor >= notaCinquenta) {
+			notaCinquenta = valor / notaCinquenta;
 			valor = valor % 50;
-			if (notaDeCinquenta != 0)
-				System.out.println(notaDeCinquenta + " notas de R$ 50,00");
+			if(notaCinquenta == 1) {
+				System.out.println(notaCinquenta + " nota de R$ 50,00");
+			}
+			else if(notaCinquenta != 0)
+				System.out.println(notaCinquenta + " notas de R$ 50,00");
 		}
 
-		if(notaDeVinte >= notaDeVinte) {
-			notaDeVinte = valor / 20;
+		if(valor >= notaVinte) {
+			notaVinte = valor / notaVinte;
 			valor = valor % 20;
-			if (notaDeVinte != 0)
-				System.out.println(notaDeVinte + " notas de R$ 20,00");
+			if(notaVinte != 1) {
+				System.out.println(notaCem + " nota de R$ 20,00");
+			}
+			else if(notaVinte != 0);
+			System.out.println(notaVinte + " notas de R$ 20,00");
 		}
 
-		if(notaDeDez >= notaDeDez) {
-			notaDeDez = valor / 10;
+		if(valor >= notaDez) {
+			notaDez = valor / notaDez;
 			valor = valor % 10;
-			if (notaDeDez != 0)
-				System.out.println(notaDeDez + " notas de R$ 10,00");
+			if(notaDez != 1) {
+				System.out.println(notaDez + " nota de R$ 10,00");
+			}
+			else if(notaDez != 0);
+			System.out.println(notaDez + " notas de R$ 10,00");
 		}
 
-		if(notaDeCinco >= notaDeCinco) {
-			notaDeCinco = valor / 5;
+		if(valor >= notaCinco) {
+			notaCinco = valor / notaCinco;
 			valor = valor % 5;
-			if (notaDeCinco != 0)
-				System.out.println(notaDeCinco + " notas de R$ 5,00");
+			if(notaCinco == 1) {
+				System.out.println(notaCinco + " nota de R$ 5,00");
+			}
+			else if(notaCinco != 0);
+			System.out.println(notaCinco + " notas de R$ 5,00");
 		}
 
-		if(notaDeDois >= notaDeDois) {
-			notaDeDois = valor / 2;
+		if(valor >= notaDois) {
+			notaDois = valor / notaDois;
 			valor = valor % 2;
-			if (notaDeDois != 0)
-				System.out.println(notaDeDois + " notas de R$ 2,00");
+			if(notaDois == 1) {
+				System.out.println(notaDois + " nota de R$ 2,00");
+			}
+			else if(notaDois != 0) 
+				System.out.println(notaDois + " notas de R$ 2,00");
 		}
 
-		if(notaDeUm >= notaDeUm)
-		{
-			notaDeUm = valor / 1;
-			valor = valor % 1;
-			if (notaDeUm != 0)
-				System.out.println(notaDeUm + " notas de R$ 1,00");
+		if(valor >= notaUm) {
+			notaUm = valor / notaUm;
+			
+			if(notaUm == 1) {
+				System.out.println(notaUm + " nota de R$ 1,00");
+			}
+			else if(notaUm != 0)
+			System.out.println(notaUm + " notas de R$ 1,00");
 		}
 	}
 }
